c90994925e81c88e9f053dd29a1b4a4e3bb53ede
buglas <1051904257@qq.com>
2022-11-12 22:19:57 +0800

    ray

---
+++ jsm/math/Ray2D.js
@@ -0,0 +1,279 @@
+import { Vector2 } from './Vector2.js'
+
+const _vector = /*@__PURE__*/ new Vector2()
+const _segCenter = /*@__PURE__*/ new Vector2()
+const _segDir = /*@__PURE__*/ new Vector2()
+const _diff = /*@__PURE__*/ new Vector2()
+
+const _edge1 = /*@__PURE__*/ new Vector2()
+const _edge2 = /*@__PURE__*/ new Vector2()
+const _normal = /*@__PURE__*/ new Vector2()
+
+class Ray2D {
+	constructor(origin = new Vector2(), direction = new Vector2(0, 0, -1)) {
+		this.origin = origin
+		this.direction = direction
+	}
+
+	set(origin, direction) {
+		this.origin.copy(origin)
+		this.direction.copy(direction)
+
+		return this
+	}
+
+	copy(ray) {
+		this.origin.copy(ray.origin)
+		this.direction.copy(ray.direction)
+
+		return this
+	}
+
+	at(t, target) {
+		return target.copy(this.direction).multiplyScalar(t).add(this.origin)
+	}
+
+	lookAt(v) {
+		this.direction.copy(v).sub(this.origin).normalize()
+
+		return this
+	}
+
+	// 相对插值值
+	recast(t) {
+		this.origin.copy(this.at(t, _vector))
+
+		return this
+	}
+
+	// 任意点在射线上的正射影
+	closestPointToPoint(point, target) {
+		target.subVectors(point, this.origin)
+
+		const directionDistance = target.dot(this.direction)
+
+		if (directionDistance < 0) {
+			return target.copy(this.origin)
+		}
+
+		return target
+			.copy(this.direction)
+			.multiplyScalar(directionDistance)
+			.add(this.origin)
+	}
+
+	// 射线到点的距离
+	distanceToPoint(point) {
+		return Math.sqrt(this.distanceSqToPoint(point))
+	}
+
+	// 射线到点的开方距离
+	distanceSqToPoint(point) {
+		const directionDistance = _vector
+			.subVectors(point, this.origin)
+			.dot(this.direction)
+
+		// point behind the ray
+
+		if (directionDistance < 0) {
+			return this.origin.distanceToSquared(point)
+		}
+
+		_vector
+			.copy(this.direction)
+			.multiplyScalar(directionDistance)
+			.add(this.origin)
+
+		return _vector.distanceToSquared(point)
+	}
+
+	// 射线到线段的开方距离？
+	distanceSqToSegment(v0, v1, optionalPointOnRay, optionalPointOnSegment) {
+		// from https://github.com/pmjoniak/GeometricTools/blob/master/GTEngine/Include/Mathematics/GteDistRaySegment.h
+		// It returns the min distance between the ray and the segment
+		// defined by v0 and v1
+		// It can also set two optional targets :
+		// - The closest point on the ray
+		// - The closest point on the segment
+
+		// 线段的中心点
+		_segCenter.copy(v0).add(v1).multiplyScalar(0.5)
+		// 线段方向
+		_segDir.copy(v1).sub(v0).normalize()
+		// 源点到线段中心的方向
+		_diff.copy(this.origin).sub(_segCenter)
+
+		// 线段长度的一半
+		const segExtent = v0.distanceTo(v1) * 0.5
+
+		// 负的射线方向与线段方向的点积
+		const a01 = -this.direction.dot(_segDir)
+
+		const b0 = _diff.dot(this.direction)
+		const b1 = -_diff.dot(_segDir)
+		const c = _diff.lengthSq()
+		const det = Math.abs(1 - a01 * a01)
+		let s0, s1, sqrDist, extDet
+
+		if (det > 0) {
+			// The ray and segment are not parallel.
+
+			s0 = a01 * b1 - b0
+			s1 = a01 * b0 - b1
+			extDet = segExtent * det
+
+			if (s0 >= 0) {
+				if (s1 >= -extDet) {
+					if (s1 <= extDet) {
+						// region 0
+						// Minimum at interior points of ray and segment.
+
+						const invDet = 1 / det
+						s0 *= invDet
+						s1 *= invDet
+						sqrDist =
+							s0 * (s0 + a01 * s1 + 2 * b0) + s1 * (a01 * s0 + s1 + 2 * b1) + c
+					} else {
+						// region 1
+
+						s1 = segExtent
+						s0 = Math.max(0, -(a01 * s1 + b0))
+						sqrDist = -s0 * s0 + s1 * (s1 + 2 * b1) + c
+					}
+				} else {
+					// region 5
+
+					s1 = -segExtent
+					s0 = Math.max(0, -(a01 * s1 + b0))
+					sqrDist = -s0 * s0 + s1 * (s1 + 2 * b1) + c
+				}
+			} else {
+				if (s1 <= -extDet) {
+					// region 4
+
+					s0 = Math.max(0, -(-a01 * segExtent + b0))
+					s1 =
+						s0 > 0 ? -segExtent : Math.min(Math.max(-segExtent, -b1), segExtent)
+					sqrDist = -s0 * s0 + s1 * (s1 + 2 * b1) + c
+				} else if (s1 <= extDet) {
+					// region 3
+
+					s0 = 0
+					s1 = Math.min(Math.max(-segExtent, -b1), segExtent)
+					sqrDist = s1 * (s1 + 2 * b1) + c
+				} else {
+					// region 2
+
+					s0 = Math.max(0, -(a01 * segExtent + b0))
+					s1 =
+						s0 > 0 ? segExtent : Math.min(Math.max(-segExtent, -b1), segExtent)
+					sqrDist = -s0 * s0 + s1 * (s1 + 2 * b1) + c
+				}
+			}
+		} else {
+			// Ray and segment are parallel.
+
+			s1 = a01 > 0 ? -segExtent : segExtent
+			s0 = Math.max(0, -(a01 * s1 + b0))
+			sqrDist = -s0 * s0 + s1 * (s1 + 2 * b1) + c
+		}
+
+		if (optionalPointOnRay) {
+			optionalPointOnRay
+				.copy(this.direction)
+				.multiplyScalar(s0)
+				.add(this.origin)
+		}
+
+		if (optionalPointOnSegment) {
+			optionalPointOnSegment.copy(_segDir).multiplyScalar(s1).add(_segCenter)
+		}
+
+		return sqrDist
+	}
+
+	// 射线在线段上的交点，Vector2|null
+	intersectSegment(p1, p2) {
+		const { origin, direction } = this
+		// 射线与线段的一般式
+		const l1 = this.getGeneralForm()
+		const l2 = this.getGeneralFormBySeg(p1, p2)
+		// 根据一般式求直线的交点
+		const interPoint = this.getInterPointOfLines(...l1, ...l2)
+		/* 没有交点 */
+		if (!interPoint) {
+			return null
+		}
+		/* 有交点 */
+		// 判断交点是否与射线同向
+		const inRay = interPoint.clone().sub(origin).dot(direction) > 0
+		if (!inRay) {
+			return null
+		}
+		// 判断交点是否在线段中
+		// 线段长度
+		const seglen = p1.lengthSq(p2)
+		// 交点到p1的距离
+		const d1 = interPoint.lengthSq(p1)
+		if (d1 > seglen) {
+			return null
+		}
+		// 交点到p2的距离
+		const d2 = interPoint.lengthSq(p2)
+		if (d2 > seglen) {
+			return null
+		}
+		return interPoint
+	}
+
+	// 一般式求交点
+	getInterPointOfLines(A1, B1, C1, A2, B2, C2) {
+		const sub = A1 * B2 - A2 * B1
+		if (sub) {
+			// 直线相交
+			return new Vector2((B1 * C2 - C1 * B2) / sub, (A2 * C1 - A1 * C2) / sub)
+		} else {
+			// 直线平行
+			return null
+		}
+	}
+
+	// 射线所在直线的一般式
+	getGeneralForm() {
+		const { origin, direction } = this
+		return this.getGeneralFormBySeg(
+			origin,
+			_vector.addVectors(origin, direction)
+		)
+	}
+
+	// 线段所在直线的一般式
+	getGeneralFormBySeg(p1, p2) {
+		const [dx, dy] = [p2.x - p1.x, p2.y - p1.y]
+		const [A, B] = [dy, -dx]
+		return [A, B, -(A * p1.x + B * p1.y)]
+	}
+
+	// 射线的反弹
+	reflect(p, n) {
+		const { origin, direction } = this
+		origin.copy(p)
+		this.direction.subVectors(
+			direction,
+			n.clone().multiplyScalar(2 * n.dot(direction))
+		)
+	}
+
+	// 求等
+	equals(ray) {
+		return (
+			ray.origin.equals(this.origin) && ray.direction.equals(this.direction)
+		)
+	}
+
+	clone() {
+		return new this.constructor().copy(this)
+	}
+}
+
+export { Ray2D }
--- jsm/math/Vector2.js
+++ jsm/math/Vector2.js
@@ -1,484 +1,381 @@
 class Vector2 {
+	constructor(x = 0, y = 0) {
+		Vector2.prototype.isVector2 = true
 
-	constructor( x = 0, y = 0 ) {
-
-		Vector2.prototype.isVector2 = true;
-
-		this.x = x;
-		this.y = y;
-
+		this.x = x
+		this.y = y
 	}
 
 	get width() {
-
-		return this.x;
-
+		return this.x
 	}
 
-	set width( value ) {
-
-		this.x = value;
-
+	set width(value) {
+		this.x = value
 	}
 
 	get height() {
-
-		return this.y;
-
+		return this.y
 	}
 
-	set height( value ) {
-
-		this.y = value;
-
+	set height(value) {
+		this.y = value
 	}
 
-	set( x, y ) {
+	set(x, y) {
+		this.x = x
+		this.y = y
 
-		this.x = x;
-		this.y = y;
-
-		return this;
-
+		return this
 	}
 
-	setScalar( scalar ) {
+	setScalar(scalar) {
+		this.x = scalar
+		this.y = scalar
 
-		this.x = scalar;
-		this.y = scalar;
-
-		return this;
-
+		return this
 	}
 
-	setX( x ) {
+	setX(x) {
+		this.x = x
 
-		this.x = x;
-
-		return this;
-
+		return this
 	}
 
-	setY( y ) {
+	setY(y) {
+		this.y = y
 
-		this.y = y;
-
-		return this;
-
+		return this
 	}
 
-	setComponent( index, value ) {
-
-		switch ( index ) {
-
-			case 0: this.x = value; break;
-			case 1: this.y = value; break;
-			default: throw new Error( 'index is out of range: ' + index );
-
+	setComponent(index, value) {
+		switch (index) {
+			case 0:
+				this.x = value
+				break
+			case 1:
+				this.y = value
+				break
+			default:
+				throw new Error('index is out of range: ' + index)
 		}
 
-		return this;
-
+		return this
 	}
 
-	getComponent( index ) {
-
-		switch ( index ) {
-
-			case 0: return this.x;
-			case 1: return this.y;
-			default: throw new Error( 'index is out of range: ' + index );
-
+	getComponent(index) {
+		switch (index) {
+			case 0:
+				return this.x
+			case 1:
+				return this.y
+			default:
+				throw new Error('index is out of range: ' + index)
 		}
-
 	}
 
 	clone() {
-
-		return new this.constructor( this.x, this.y );
-
+		return new this.constructor(this.x, this.y)
 	}
 
-	copy( v ) {
+	copy(v) {
+		this.x = v.x
+		this.y = v.y
 
-		this.x = v.x;
-		this.y = v.y;
-
-		return this;
-
+		return this
 	}
 
-	add( v, w ) {
-
-		if ( w !== undefined ) {
-
-			console.warn( 'THREE.Vector2: .add() now only accepts one argument. Use .addVectors( a, b ) instead.' );
-			return this.addVectors( v, w );
-
+	add(v, w) {
+		if (w !== undefined) {
+			console.warn(
+				'THREE.Vector2: .add() now only accepts one argument. Use .addVectors( a, b ) instead.'
+			)
+			return this.addVectors(v, w)
 		}
 
-		this.x += v.x;
-		this.y += v.y;
+		this.x += v.x
+		this.y += v.y
 
-		return this;
-
+		return this
 	}
 
-	addScalar( s ) {
+	addScalar(s) {
+		this.x += s
+		this.y += s
 
-		this.x += s;
-		this.y += s;
-
-		return this;
-
+		return this
 	}
 
-	addVectors( a, b ) {
+	addVectors(a, b) {
+		this.x = a.x + b.x
+		this.y = a.y + b.y
 
-		this.x = a.x + b.x;
-		this.y = a.y + b.y;
-
-		return this;
-
+		return this
 	}
 
-	addScaledVector( v, s ) {
+	addScaledVector(v, s) {
+		this.x += v.x * s
+		this.y += v.y * s
 
-		this.x += v.x * s;
-		this.y += v.y * s;
-
-		return this;
-
+		return this
 	}
 
-	sub( v, w ) {
-
-		if ( w !== undefined ) {
-
-			console.warn( 'THREE.Vector2: .sub() now only accepts one argument. Use .subVectors( a, b ) instead.' );
-			return this.subVectors( v, w );
-
+	sub(v, w) {
+		if (w !== undefined) {
+			console.warn(
+				'THREE.Vector2: .sub() now only accepts one argument. Use .subVectors( a, b ) instead.'
+			)
+			return this.subVectors(v, w)
 		}
 
-		this.x -= v.x;
-		this.y -= v.y;
+		this.x -= v.x
+		this.y -= v.y
 
-		return this;
-
+		return this
 	}
 
-	subScalar( s ) {
+	subScalar(s) {
+		this.x -= s
+		this.y -= s
 
-		this.x -= s;
-		this.y -= s;
-
-		return this;
-
+		return this
 	}
 
-	subVectors( a, b ) {
+	subVectors(a, b) {
+		this.x = a.x - b.x
+		this.y = a.y - b.y
 
-		this.x = a.x - b.x;
-		this.y = a.y - b.y;
-
-		return this;
-
+		return this
 	}
 
-	multiply( v ) {
+	multiply(v) {
+		this.x *= v.x
+		this.y *= v.y
 
-		this.x *= v.x;
-		this.y *= v.y;
-
-		return this;
-
+		return this
 	}
 
-	multiplyScalar( scalar ) {
+	multiplyScalar(scalar) {
+		this.x *= scalar
+		this.y *= scalar
 
-		this.x *= scalar;
-		this.y *= scalar;
-
-		return this;
-
+		return this
 	}
 
-	divide( v ) {
+	divide(v) {
+		this.x /= v.x
+		this.y /= v.y
 
-		this.x /= v.x;
-		this.y /= v.y;
-
-		return this;
-
+		return this
 	}
 
-	divideScalar( scalar ) {
-
-		return this.multiplyScalar( 1 / scalar );
-
+	divideScalar(scalar) {
+		return this.multiplyScalar(1 / scalar)
 	}
 
-	applyMatrix3( m ) {
+	applyMatrix3(m) {
+		const x = this.x,
+			y = this.y
+		const e = m.elements
 
-		const x = this.x, y = this.y;
-		const e = m.elements;
+		this.x = e[0] * x + e[3] * y + e[6]
+		this.y = e[1] * x + e[4] * y + e[7]
 
-		this.x = e[ 0 ] * x + e[ 3 ] * y + e[ 6 ];
-		this.y = e[ 1 ] * x + e[ 4 ] * y + e[ 7 ];
-
-		return this;
-
+		return this
 	}
 
-	min( v ) {
+	min(v) {
+		this.x = Math.min(this.x, v.x)
+		this.y = Math.min(this.y, v.y)
 
-		this.x = Math.min( this.x, v.x );
-		this.y = Math.min( this.y, v.y );
-
-		return this;
-
+		return this
 	}
 
-	max( v ) {
+	max(v) {
+		this.x = Math.max(this.x, v.x)
+		this.y = Math.max(this.y, v.y)
 
-		this.x = Math.max( this.x, v.x );
-		this.y = Math.max( this.y, v.y );
-
-		return this;
-
+		return this
 	}
 
-	clamp( min, max ) {
-
+	clamp(min, max) {
 		// assumes min < max, componentwise
 
-		this.x = Math.max( min.x, Math.min( max.x, this.x ) );
-		this.y = Math.max( min.y, Math.min( max.y, this.y ) );
+		this.x = Math.max(min.x, Math.min(max.x, this.x))
+		this.y = Math.max(min.y, Math.min(max.y, this.y))
 
-		return this;
-
+		return this
 	}
 
-	clampScalar( minVal, maxVal ) {
+	clampScalar(minVal, maxVal) {
+		this.x = Math.max(minVal, Math.min(maxVal, this.x))
+		this.y = Math.max(minVal, Math.min(maxVal, this.y))
 
-		this.x = Math.max( minVal, Math.min( maxVal, this.x ) );
-		this.y = Math.max( minVal, Math.min( maxVal, this.y ) );
-
-		return this;
-
+		return this
 	}
 
-	clampLength( min, max ) {
+	clampLength(min, max) {
+		const length = this.length()
 
-		const length = this.length();
-
-		return this.divideScalar( length || 1 ).multiplyScalar( Math.max( min, Math.min( max, length ) ) );
-
+		return this.divideScalar(length || 1).multiplyScalar(
+			Math.max(min, Math.min(max, length))
+		)
 	}
 
 	floor() {
+		this.x = Math.floor(this.x)
+		this.y = Math.floor(this.y)
 
-		this.x = Math.floor( this.x );
-		this.y = Math.floor( this.y );
-
-		return this;
-
+		return this
 	}
 
 	ceil() {
+		this.x = Math.ceil(this.x)
+		this.y = Math.ceil(this.y)
 
-		this.x = Math.ceil( this.x );
-		this.y = Math.ceil( this.y );
-
-		return this;
-
+		return this
 	}
 
 	round() {
+		this.x = Math.round(this.x)
+		this.y = Math.round(this.y)
 
-		this.x = Math.round( this.x );
-		this.y = Math.round( this.y );
-
-		return this;
-
+		return this
 	}
 
 	roundToZero() {
+		this.x = this.x < 0 ? Math.ceil(this.x) : Math.floor(this.x)
+		this.y = this.y < 0 ? Math.ceil(this.y) : Math.floor(this.y)
 
-		this.x = ( this.x < 0 ) ? Math.ceil( this.x ) : Math.floor( this.x );
-		this.y = ( this.y < 0 ) ? Math.ceil( this.y ) : Math.floor( this.y );
-
-		return this;
-
+		return this
 	}
 
 	negate() {
+		this.x = -this.x
+		this.y = -this.y
 
-		this.x = - this.x;
-		this.y = - this.y;
-
-		return this;
-
+		return this
 	}
 
-	dot( v ) {
-
-		return this.x * v.x + this.y * v.y;
-
+	dot(v) {
+		return this.x * v.x + this.y * v.y
 	}
 
-	cross( v ) {
-
-		return this.x * v.y - this.y * v.x;
-
+	cross(v) {
+		return this.x * v.y - this.y * v.x
 	}
 
 	lengthSq() {
-
-		return this.x * this.x + this.y * this.y;
-
+		return this.x * this.x + this.y * this.y
 	}
 
 	length() {
-
-		return Math.sqrt( this.x * this.x + this.y * this.y );
-
+		return Math.sqrt(this.x * this.x + this.y * this.y)
 	}
 
 	manhattanLength() {
-
-		return Math.abs( this.x ) + Math.abs( this.y );
-
+		return Math.abs(this.x) + Math.abs(this.y)
 	}
 
 	normalize() {
-
-		return this.divideScalar( this.length() || 1 );
-
+		return this.divideScalar(this.length() || 1)
 	}
 
 	angle() {
-
 		// computes the angle in radians with respect to the positive x-axis
 
-		const angle = Math.atan2( - this.y, - this.x ) + Math.PI;
+		const angle = Math.atan2(-this.y, -this.x) + Math.PI
 
-		return angle;
-
+		return angle
 	}
 
-	distanceTo( v ) {
-
-		return Math.sqrt( this.distanceToSquared( v ) );
-
+	distanceTo(v) {
+		return Math.sqrt(this.distanceToSquared(v))
 	}
 
-	distanceToSquared( v ) {
-
-		const dx = this.x - v.x, dy = this.y - v.y;
-		return dx * dx + dy * dy;
-
+	distanceToSquared(v) {
+		const dx = this.x - v.x,
+			dy = this.y - v.y
+		return dx * dx + dy * dy
 	}
 
-	manhattanDistanceTo( v ) {
-
-		return Math.abs( this.x - v.x ) + Math.abs( this.y - v.y );
-
+	manhattanDistanceTo(v) {
+		return Math.abs(this.x - v.x) + Math.abs(this.y - v.y)
 	}
 
-	setLength( length ) {
-
-		return this.normalize().multiplyScalar( length );
-
+	setLength(length) {
+		return this.normalize().multiplyScalar(length)
 	}
 
-	lerp( v, alpha ) {
+	lerp(v, alpha) {
+		this.x += (v.x - this.x) * alpha
+		this.y += (v.y - this.y) * alpha
 
-		this.x += ( v.x - this.x ) * alpha;
-		this.y += ( v.y - this.y ) * alpha;
-
-		return this;
-
+		return this
 	}
 
-	lerpVectors( v1, v2, alpha ) {
+	lerpVectors(v1, v2, alpha) {
+		this.x = v1.x + (v2.x - v1.x) * alpha
+		this.y = v1.y + (v2.y - v1.y) * alpha
 
-		this.x = v1.x + ( v2.x - v1.x ) * alpha;
-		this.y = v1.y + ( v2.y - v1.y ) * alpha;
-
-		return this;
-
+		return this
 	}
 
-	equals( v ) {
-
-		return ( ( v.x === this.x ) && ( v.y === this.y ) );
-
+	equals(v) {
+		return v.x === this.x && v.y === this.y
 	}
 
-	fromArray( array, offset = 0 ) {
+	fromArray(array, offset = 0) {
+		this.x = array[offset]
+		this.y = array[offset + 1]
 
-		this.x = array[ offset ];
-		this.y = array[ offset + 1 ];
-
-		return this;
-
+		return this
 	}
 
-	toArray( array = [], offset = 0 ) {
+	toArray(array = [], offset = 0) {
+		array[offset] = this.x
+		array[offset + 1] = this.y
 
-		array[ offset ] = this.x;
-		array[ offset + 1 ] = this.y;
-
-		return array;
-
+		return array
 	}
 
-	fromBufferAttribute( attribute, index, offset ) {
-
-		if ( offset !== undefined ) {
-
-			console.warn( 'THREE.Vector2: offset has been removed from .fromBufferAttribute().' );
-
+	fromBufferAttribute(attribute, index, offset) {
+		if (offset !== undefined) {
+			console.warn(
+				'THREE.Vector2: offset has been removed from .fromBufferAttribute().'
+			)
 		}
 
-		this.x = attribute.getX( index );
-		this.y = attribute.getY( index );
+		this.x = attribute.getX(index)
+		this.y = attribute.getY(index)
 
-		return this;
-
+		return this
 	}
 
-	rotateAround( center, angle ) {
+	rotateAround(center, angle) {
+		const c = Math.cos(angle),
+			s = Math.sin(angle)
 
-		const c = Math.cos( angle ), s = Math.sin( angle );
+		const x = this.x - center.x
+		const y = this.y - center.y
 
-		const x = this.x - center.x;
-		const y = this.y - center.y;
+		this.x = x * c - y * s + center.x
+		this.y = x * s + y * c + center.y
 
-		this.x = x * c - y * s + center.x;
-		this.y = x * s + y * c + center.y;
-
-		return this;
-
+		return this
 	}
 
 	random() {
+		this.x = Math.random()
+		this.y = Math.random()
 
-		this.x = Math.random();
-		this.y = Math.random();
-
-		return this;
-
+		return this
 	}
 
-	*[ Symbol.iterator ]() {
-
-		yield this.x;
-		yield this.y;
-
+	*[Symbol.iterator]() {
+		yield this.x
+		yield this.y
 	}
-
 }
 
-export { Vector2 };
+export { Vector2 }
